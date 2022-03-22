79dbb7497ccf35e9fbea95a983738fcd5fcb3370
Piotr Radecki <radecki226@gmail.com>
2022-03-22 16:54:57 +0100

    receiver tb

--- verif/uart_verif/uart_receiver_tb.sv
+++ verif/uart_verif/uart_receiver_tb.sv
@@ -15,19 +15,21 @@
   `define RECV_RATE 115000
 `endif
 `define RECV_DELAY 1000000000/(2*`RECV_RATE)
+`ifndef ITERATION_DELAY
+  `define ITERATION_DELAY 2
+`endif
  
 /*Assumption is that input clk has f=10MHz*/
 module tb;
 	
 	bit [8:0] out_dat_buffer [`N_DATA:0];
-	bit [2:0] out_overhead_buffer [`N_DATA:0];
 
 	bit [8:0] rx_dat_buffer [`N_DATA:0];
-	bit [2:0] rx_overhead_buffer [`N_DATA:0];
+	bit       rx_driver;
 
-	int tx_written = 0;
+	int out_dat_written = 0;
 	int error_cnt = 0;
-	int sc_i = 0;
+	int finish_flag = 0;
 	
 	reg external_clk;
 
@@ -53,8 +55,6 @@ module tb;
 
 	wire      tx;
 	reg       rx;
-	int       state;
-	int       drv_dat;
 
 
 	/*DUT declaration*/
@@ -101,13 +101,14 @@ module tb;
 		rst <= 0;
 		tx_en <= 0;
 		rx_en <= 1;
+		rx <= 1;
 		n_parity_bits <= `N_PARITY_BITS;
 		n_stop_bits <= `N_STOP_BITS;
 		n_data_bits <= `N_BITS;
 		internal_clk_divider <= 87; /*115200*/ /*div - 87:*/
 		
 		
-		#10000000 
+		/*#10000000 
 		if (error_cnt > 0) begin
 			$display("Sumlation FAILED!");
 		end else begin
@@ -115,7 +116,7 @@ module tb;
 		end
 		$display("test %d\n", `TEST);
 		$display("delay %d\n", `RECV_DELAY);
-		$finish;
+		$finish;*/
 
 	end
 
@@ -125,115 +126,81 @@ module tb;
 		$dumpvars;
 	end
 
-	/*write (in_dat) generator and driver*/
+	/*rx dat generator*/
 	initial begin
-		in_vld <= 0;
-		drv_dat = 1;
-		#340
-		in_vld <= 1;
-		rx_dat <= $urandom();
-		for (int i = 0; i < n_writes; i++) begin
-			$display("T = %0t [generator] frame idx: 0%0d",$time ,i);
-			drv_dat = 1;
-			in_vld = 1;
-			@(drv_dat == 0);			
+		for (int i = 0; i < `N_DATA; i++) begin
+			rx_dat_buffer[i] <= $urandom();
 		end
-		in_vld <= 0;
 	end
 
 	/*driver*/
-	always @(posedge clk) begin
-		if (drv_dat == 1 && in_rdy == 1 && in_vld == 1) begin
-			in_dat[8] <= 0;
-			in_dat[7:0] <= $urandom();
-			drv_dat <= 0;
-		end
-	end
-
-
-	/*monitor in_dat_part*/
 	initial begin
-		reg [2:0] in_overhead_capture; 
-		for (int i = 0; i < n_writes; i++) begin
-			@(posedge clk);
-			
-			if (in_rdy == 1 && in_vld == 1) begin
-				in_dat_buffer[i] = in_dat[`N_BITS-1:0];
-	
-				in_overhead_capture = 3'b010;
-				if (n_parity_bits == 1'b1) begin
-					in_overhead_capture[0] = ^in_dat[`N_BITS-1:0];
-				end	
-				if (n_stop_bits == 2) begin 
-					in_overhead_capture[2:1] = 2'b11; 
-				end
-				in_overhead_buffer[i] = in_overhead_capture;
-
-			end else begin
-				i = i-1;
-			end
-		end
-	end
-
-	/*monitor in tx part*/
-	initial begin
-		@(tx == 1);
-		for (int i = 0; i < n_writes; i++) begin 
-			@(tx == 0);
+		#100
+		for (int i = 0; i < `N_DATA; i++) begin
+      #`ITERATION_DELAY
+			rx <= 0;
 			@(posedge external_clk);
-			tx_dat_capture =  0;
-			tx_parity_capture = 0;
-			tx_stop_capture = 0;
-			state = 1;
-			for (int j = 0; j < n_data_bits; j++) begin
+			for (int j = 0; j < `N_BITS; j++) begin
+				rx <= rx_dat_buffer[i][j];
 				@(posedge external_clk);
-				state = 2;
-				tx_dat_capture[j] = tx;
 			end
-			for (int j = 0; j < n_parity_bits; j++) begin
+			if (`N_PARITY_BITS == 1) begin
+			  rx <= ^rx_dat_buffer[i];
 				@(posedge external_clk);
-				tx_parity_capture = tx;
-				state = 3;
 			end
-			for (int j = 0; j < n_stop_bits; j++) begin 
+			for (int j = 0; j < `N_STOP_BITS; j++) begin
+				rx <= 1;
 				@(posedge external_clk);
-				tx_stop_capture[j] = tx;
-				state = 4;
 			end
-			tx_dat_buffer[i] = tx_dat_capture;
-			tx_overhead_buffer[i] = {tx_stop_capture,tx_parity_capture};
-			tx_written = 1;
+      $display("T=%0t [Driver] Data with addr = 0x%0h and value = 0x%0h has been driven", 
+			         $time, i, rx_dat_buffer[i]);
+
 		end
 	end
 
+  /*Monitor out_dat*/
+	initial begin
+		for (int i = 0; i < `N_DATA; i++) begin
+			@(out_vld == 1);
+			@(posedge clk);
+			out_dat_buffer[i] = out_dat;
+			out_dat_written = 1;
+			@(out_dat_written == 0);
+		end
+	end
 	/*scoreboard*/
 	
 	initial begin
-		for (sc_i = 0; sc_i < n_writes; sc_i++) begin
-			@(tx_written == 1);
-			tx_written = 0;
-			if (tx_dat_buffer[sc_i] == in_dat_buffer[sc_i]) begin
-        		if (tx_overhead_buffer[sc_i] == in_overhead_buffer[sc_i]) begin
-            		$display("T=%0t [Scoreboard] PASS! addr = 0x%0h expected = 0x%0h received = 0x%0h", 
-					         $time, sc_i, in_dat_buffer[sc_i], tx_dat_buffer[sc_i]);
-				end else begin
-					$display("T=%0t [Scoreboard] ERROR! overhead mismatch addr = 0x%0h expected = %0b received = %0b",
-				         	$time, sc_i, in_overhead_buffer[sc_i], tx_overhead_buffer[sc_i]);
-					error_cnt = error_cnt + 1;
-				end 
+		for (int i = 0; i < `N_DATA; i++) begin
+			@(out_dat_written == 1);
+			if (out_dat_buffer[i][`N_BITS-1:0] == rx_dat_buffer[i][`N_BITS-1:0]) begin
+        $display("T=%0t [Scoreboard] PASS! addr = 0x%0h expected = 0x%0h received = 0x%0h", 
+			           $time, i, out_dat_buffer[i], rx_dat_buffer[i]);
 			end else begin
 				$display("T=%0t [Scoreboard] ERROR! data mismatch addr = 0x%0h expected = 0x%0h received = 0x%0h",
-				         $time, sc_i, in_dat_buffer[sc_i], tx_dat_buffer[sc_i]);
+				         $time, i, out_dat_buffer[i], rx_dat_buffer[i]);
+				error_cnt = error_cnt + 1;
 			end
+			out_dat_written = 1;
 		end
+		finish_flag = 1;
 	end
 	
 	initial begin
-		#10000000	
-		if(sc_i < n_writes-1) begin
-    		$display("T=%0t [Scoreboard] ERROR! Not all data has been captured. Captured frames: %0d", $time, sc_i);
-			error_cnt = error_cnt + 1;
-		end
+		#100000000	
+    $display("T=%0t [Scoreboard] ERROR! Not all data has been captured",$time);
+	  error_cnt = error_cnt + 1;
+		finish_flag = 1;
+	end
+
+	initial begin
+		@(finish_flag == 1);
+		if (error_cnt > 0) begin
+			$display("Simulation FAILED!");
+		end else begin
+			$display("Simulation PASSED!");
+	  end
+		$finish;
 	end
 	
 
